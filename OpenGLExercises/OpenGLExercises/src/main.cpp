f73c1e40355a6510d1109096967e224802dfac25
xWintx <drwint_bkhn@yahoo.com>
2018-03-23 16:13:10 +0700

    Initial commit

---
+++ OpenGLExercises/OpenGLExercises/src/main.cpp
@@ -0,0 +1,59 @@
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+void frame_buffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main()
+{
+	//Init GLFW
+	glfwInit();
+	//Configure OpenGL options
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//-------------
+
+	//Create window object
+	GLFWwindow* window = glfwCreateWindow(800, 600, "HelloOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	//-------------
+
+	//Init GLAD before calling any OpenGL function
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	//-------------
+
+	//Create a viewport to render
+	glViewport(0, 0, 800, 600);
+	//Register callback function handle window's buffer size changes
+	glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+	//Render loop - keep running till window should stop
+	while (!glfwWindowShouldClose(window))
+	{
+		//Swap color buffer used as output to the screen
+		glfwSwapBuffers(window);
+		//Handle any events are triggered
+		glfwPollEvents();
+	}
+	//--------------
+
+	//Clean up before exit
+	glfwTerminate();
+
+	//Application end
+	return 0;
+}
