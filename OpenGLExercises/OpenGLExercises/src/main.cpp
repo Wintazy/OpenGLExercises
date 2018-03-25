7161f343b6dec4b509d8de898d9fc6bff90e873a
xWintx <drwint_bkhn@yahoo.com>
2018-03-25 18:44:28 +0700

    - Finish triangle rendering part

--- OpenGLExercises/OpenGLExercises/src/main.cpp
+++ OpenGLExercises/OpenGLExercises/src/main.cpp
@@ -1,19 +1,25 @@
 #include <glad\glad.h>
 #include <GLFW\glfw3.h>
+
 #include <iostream>
 
-void frame_buffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
+void frame_buffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow *window);
+int compileShader(const char* shaderSource, GLuint type);
 
-void processInput(GLFWwindow *window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	{
-		glfwSetWindowShouldClose(window, true);
-	}
-}
+const char *vertexShaderSource = "#version 330\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char *fragmentShaderSource = "#version 330\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
 
 int main()
 {
@@ -45,10 +51,75 @@ int main()
 	}
 	//-------------
 
-	//Create a viewport to render
-	glViewport(0, 0, 800, 600);
+	//Set draw style to wireframe polygons
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 	//Register callback function handle window's buffer size changes
 	glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
+
+	//Compile shader
+	int vertexShader = compileShader(vertexShaderSource, GL_VERTEX_SHADER);
+	int fragmentShader = compileShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
+	//------
+
+	//Link shader
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader( shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	int success;
+	char infoLog[512];
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "Failed to link shader program!\n" << infoLog << std::endl;
+	}
+
+	//Delete shader after linked
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	//-----
+
+	//prepare vertex data to render
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.5f, 0.5f, 0.0f,
+		-0.5f, 0.5f, 0.0f
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		0, 2, 3
+	};
+
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	//Bind vertext buffer and set buffer data
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//Tell OpenGL how the hell it should read the vertex attributes data
+	//Here is: the vertext attribute at location 0, have 3 values, type is FLOAT, shouldn't normalize,
+	//size of the stride to next vertex atrribute set is 3*sizeof(float),
+	//and offset of where the data begins in the buffer is 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//Remember to enable these attribute at that location before render
+	glEnableVertexAttribArray(0);
+
+	//Buffer could be Unbound after the attributes were registered
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	//So is the vertex array
+	glBindVertexArray(0);
 
 	//Render loop - keep running till window should stop
 	while (!glfwWindowShouldClose(window))
@@ -61,6 +132,13 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 0.1f);
 		//Clear color buffer
 		glClear(GL_COLOR_BUFFER_BIT);
+		//Draw triangle part
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 		/*---Rendering end---*/
 
 		//Swap color buffer used as output to the screen
@@ -71,8 +149,42 @@ int main()
 	//--------------
 
 	//Clean up before exit
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 	glfwTerminate();
 
 	//Application end
 	return 0;
+}
+
+void frame_buffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int compileShader(const char* shaderSource, GLuint type)
+{
+	int shaderResult = glCreateShader(type);
+	glShaderSource(shaderResult, 1, &shaderSource, NULL);
+	glCompileShader(shaderResult);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shaderResult, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shaderResult, 512, NULL, infoLog);
+		std::cout << "Failed to compile shader!\n" << infoLog << std::endl;
+		return -1;
+	}
+
+	return shaderResult;
 }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # My OpenGL Exercises
 
 These exercises based on https://learnopengl.com
+
+## 1st exercise: Our program basic life cycle
+
+![./Docs/Image/OpenGLBasicProgram.svg](./Docs/Image/OpenGLBasicProgram.svg)
+
+## 2st exercise: OpenGL pipeline
+
+![./Docs/Image/OpenGLPipeline.svg](./Docs/Image/OpenGLPipeline.svg)
